fdc1455fdfdc365f8aeea7e367328a95630a9fef
GahelMqz <gaelaldairocho@gmail.com>
2025-11-06 19:36:34 -0500

    test

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,16 @@
-self.addEventListener("push", event => {
-  const data = event.data?.json() || {};
-  event.waitUntil(
-    self.registration.showNotification(data.title || "Notificación", {
-      body: data.body || "Mensaje recibido",
-      icon: "/vite.svg"
-    })
-  );
+// self.addEventListener("push", event => {
+//   const data = event.data?.json() || {};
+//   event.waitUntil(
+//     self.registration.showNotification(data.title || "Notificación", {
+//       body: data.body || "Mensaje recibido",
+//       icon: "/vite.svg"
+//     })
+//   );
+// });
+
+
+const registration = await navigator.serviceWorker.ready;
+registration.showNotification("¡Has lanzado una Pokéball!", {
+  body: "¡Un Pokémon salvaje apareció!",
+  icon: "/vite.svg"
 });
--- src/components/NotificationButton.jsx
+++ src/components/NotificationButton.jsx
@@ -1,20 +1,35 @@
 import React from "react";
 
 export default function NotificationButton() {
-  const handleClick = async () => {
-    // 1. Pedir permiso
-    const permission = await Notification.requestPermission();
-    if (permission !== "granted") {
-      alert("Permiso denegado para notificaciones");
-      return;
-    }
+  // const handleClick = async () => {
+  //   // 1. Pedir permiso
+  //   const permission = await Notification.requestPermission();
+  //   if (permission !== "granted") {
+  //     alert("Permiso denegado para notificaciones");
+  //     return;
+  //   }
 
-    // 2. Mostrar notificación local
-    new Notification("Haz lanzado una Pokeball", {
-      body: "Esta es una notificación generada al hacer click",
-      icon: "/pokemon.png"
-    });
-  };
+  //   // 2. Mostrar notificación local
+  //   new Notification("Haz lanzado una Pokeball", {
+  //     body: "Esta es una notificación generada al hacer click",
+  //     icon: "/pokemon.png"
+  //   });
+  // };
+
+  const handleClick = async () => {
+  const permission = await Notification.requestPermission();
+  if (permission !== "granted") {
+    alert("Permiso denegado para notificaciones");
+    return;
+  }
+
+  const registration = await navigator.serviceWorker.ready;
+  registration.showNotification("¡Has lanzado una Pokéball! 🎉", {
+    body: "¡Un Pokémon salvaje apareció!",
+    icon: "/vite.svg"
+  });
+};
+
 
   return (
     <button className="pokeball" onClick={handleClick}>
